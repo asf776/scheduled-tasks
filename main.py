0c618b617daa8a0dae12957113fe098c12152e71
asf776 <asf77681@gmail.com>
2026-03-26 15:44:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from dotenv import find_dotenv, load_dotenv
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+path = find_dotenv(".env.txt",True)
+load_dotenv(path)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+api_key = os.getenv("OWM_API_KEY") # security, or modifying recepients of mail lists, etc.
+lat = 51.690090
+long = 5.303690
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+url = "https://api.openweathermap.org/data/2.5/forecast"
+params = {
+    "lat": lat,
+    "lon": long,
+    "appid": api_key,
+    "units": "metric",
+    "cnt": 4,
+}
+
+response = requests.get(url, params=params)
+response.raise_for_status()
+data = response.json()
+print(f"Status code: {data["cod"]}\n")
+
+no_intervals = len(data["list"])
+will_rain = False
+descriptions = []
+for interval in range(no_intervals):
+    for condition in range(len(data["list"][interval]["weather"])):
+        descriptions.append([data["list"][interval]["weather"][0][
+                  "description"]," ",f"({str(data["list"][interval]["dt_txt"].split()[1].split(":")[0])}h)"]) # second 0 can also have higher indices for additional conditions
+        if interval != no_intervals-1:
+            descriptions.append(", ")
+        if int(data["list"][interval]["weather"][condition]["id"]) < 700:
+            will_rain = True
+
+descriptions_n = ["".join(i) for i in descriptions] # joins each interval to a full string
+print("\n")
+
+import smtplib
+
+my_email = os.getenv("my_email")
+password = os.getenv("email_password")
+
+if will_rain:
+    with smtplib.SMTP("smtp.gmail.com",
+                      port=587) as connection:  # this code belongs to the email of the SENDER, not recipient
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email,
+                            msg=f"Subject:Rain predicted\n\nBring an umbrella today!\nForecast: {"".join(descriptions_n)}")
